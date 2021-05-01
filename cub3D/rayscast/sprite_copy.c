3afa63a82841ce49f59d8a5378ef9d9cba47d22a
ycordell <ycordell@student.21-school.ru>
2021-05-01 18:33:37 +0300

    норма 2

--- cub3D/rayscast/sprite_copy.c
+++ cub3D/rayscast/sprite_copy.c
@@ -6,7 +6,7 @@
 /*   By: ycordell <ycordell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/01 17:30:19 by ycordell          #+#    #+#             */
-/*   Updated: 2021/05/01 18:20:03 by ycordell         ###   ########.fr       */
+/*   Updated: 2021/05/01 18:32:55 by ycordell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,33 +52,40 @@ void	sp_1(t_all *all)
 	}
 }
 
-void sp_drawstart(t_all *all, int i)
+void	sp_drawstart(t_all *all, int i)
 {
-	all->sprite->invDet = 1.0 / (all->param_map->planeX * all->param_map->dirY - all->param_map->dirX * all->param_map->planeY);
-    all->sprite->spriteX = (double)all->sprite->sp[all->sprite->sp_order[i]].x  - all->param_map->posX;
-    all->sprite->spriteY = all->sprite->sp[all->sprite->sp_order[i]].y - all->param_map->posY;
-    all->sprite->transformX  = all->sprite->invDet 
-		* (all->param_map->dirY * all->sprite->spriteX - all->param_map->dirX * all->sprite->spriteY);
-    all->sprite->transformY = all->sprite->invDet 
-		* (-all->param_map->planeY * all->sprite->spriteX + all->param_map->planeX * all->sprite->spriteY);
-		all->sprite->sp_dist[i] = all->sprite->transformY;
-        all->sprite->spriteScreenX = (int)((all->param_map->scr_w / 2) * (1 + all->sprite->transformX  / all->sprite->transformY));
-        all->sprite->spriteHeight = abs((int)(all->param_map->scr_h / (all->sprite->transformY)));
- 		all->sprite->drawStartY = -all->sprite->spriteHeight / 2 + all->param_map->scr_h / 2; 
-		if(all->sprite->drawStartY < 0)
-            all->sprite->drawStartY = 0;
+	all->sprite->invDet = 1.0 / (all->param_map->planeX * all->param_map->dirY
+			- all->param_map->dirX * all->param_map->planeY);
+	all->sprite->spriteX = (double)all->sprite->sp[all->sprite->sp_order[i]].x
+		- all->param_map->posX;
+	all->sprite->spriteY = all->sprite->sp[all->sprite->sp_order[i]].y
+		- all->param_map->posY;
+	all->sprite->transformX = all->sprite->invDet
+		* (all->param_map->dirY * all->sprite->spriteX - all->param_map->dirX
+			* all->sprite->spriteY);
+	all->sprite->transformY = all->sprite->invDet
+		* (-all->param_map->planeY * all->sprite->spriteX
+			+ all->param_map->planeX * all->sprite->spriteY);
+	all->sprite->sp_dist[i] = all->sprite->transformY;
+	all->sprite->spriteScreenX = (int)((all->param_map->scr_w / 2)
+			* (1 + all->sprite->transformX / all->sprite->transformY));
+	all->sprite->spriteHeight = abs((int)(all->param_map->scr_h
+				/ (all->sprite->transformY)));
+	all->sprite->drawStartY = -all->sprite->spriteHeight / 2
+		+ all->param_map->scr_h / 2;
+	if (all->sprite->drawStartY < 0)
+		all->sprite->drawStartY = 0;
 }
 
 void	sp_drawend(t_all *all)
 {
-
 	all->sprite->drawEndY = all->sprite->spriteHeight / 2 + all->param_map->scr_h / 2;
     if(all->sprite->drawEndY >= all->param_map->scr_h)
         all->sprite->drawEndY = all->param_map->scr_h - 1;
     all->sprite->spriteWidth = abs((int)(all->param_map->scr_h / (all->sprite->transformY)));
-    all->sprite->drawStartX= -all->sprite->spriteWidth / 2 + all->sprite->spriteScreenX;
-    if(all->sprite->drawStartX< 0)
-        all->sprite->drawStartX= 0;
+    all->sprite->drawStartX = -all->sprite->spriteWidth / 2 + all->sprite->spriteScreenX;
+    if(all->sprite->drawStartX < 0)
+        all->sprite->drawStartX = 0;
     all->sprite->drawEndX = all->sprite->spriteWidth / 2 + all->sprite->spriteScreenX;
     if(all->sprite->drawEndX >= all->param_map->scr_w)
         all->sprite->drawEndX = all->param_map->scr_w - 1;
